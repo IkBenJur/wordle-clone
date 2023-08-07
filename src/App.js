13f194aa50a40150229fa26e91d4eb3029e648a4
--global <--global>
2023-08-07 15:20:45 +0200

    Guess now displayed in line. Cant be longer than 5

--- src/App.js
+++ src/App.js
@@ -3,30 +3,39 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [solution, setSolution] = useState("percy")
-  const [guesses, setGuess] = useState(Array(6).fill(null))
+  const [guesses, setGuesses] = useState(Array(6).fill(null))
+  const [currentGuess, setCurrentguess] = useState("")
 
-  function onKeyPressed (e) {
-    const char = e.key
-    const regex = /^[a-z]$/;
-
-    //Listen to only a to z
-    if (regex.test(char)){
-      console.log(char)
-    }
-  }
+  
 
   useEffect(() => {
     //API Request for word
     // const randomWordFromAPI =
     // setSolution(randomWordFromAPI)
 
-    document.addEventListener("keydown", onKeyPressed)
-    
-    return () => {
-      document.removeEventListener("keydown", onKeyPressed)
+    function onKeyPressed (e) {
+      const char = e.key
+      const regex = /^[a-z]$/;
+  
+      //When pressed enter and when chars is not greater than 5 submit to guess
+      if (char === 'Enter' && currentGuess.length === 5){
+        
+
+      }
+
+      //Listen to only a to z
+      if (regex.test(char) ){
+        //Generate word of user input
+        setCurrentguess(currentGuess.length === 5 ? currentGuess : currentGuess + char)
+      }
     }
 
-  },[])
+    window.addEventListener("keydown", onKeyPressed)
+    
+    return () => {
+      window.removeEventListener("keydown", onKeyPressed)
+    }
+  },[currentGuess, guesses])
   
   const Line = ({ guess }) => {
     const boxes = []
@@ -44,7 +53,8 @@ function App() {
     <div className='game'> 
       <h1>Wordle clone</h1>
       {guesses.map((guess, idx) => {
-        return <Line key={idx} guess={guess ? guess: ""} />
+        const isCurrenGuess = idx === guesses.findIndex(value => !value)
+        return <Line key={idx} guess={isCurrenGuess ? currentGuess : guess ? guess: ""} />
       })}
     </div>
   );
